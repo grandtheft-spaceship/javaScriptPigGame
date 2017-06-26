1ee3e302b987b95a9abc9d0a1b4652b61a440bbf
grandtheft-spaceship <justdizon71@gmail.com>
2017-06-26 06:40:40 -0700

    :art: Completes functionality to update global scores with 'hold' button and determine winner

--- app.js
+++ app.js
@@ -41,19 +41,41 @@ document.querySelector( ".btn-roll" ).addEventListener( "click", function() {
     document.querySelector( "#current-" + activePlayer ).textContent = roundScore;
   } else {
     // Next Player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById( "current-0" ).textContent = "0";
-    document.getElementById( "current-1" ).textContent = "0";
-
-    // document.querySelector( ".player-0-panel" ).classList.remove( "active" );
-    // document.querySelector( ".player-1-panel" ).classList.add( "active" );
-
-    document.querySelector( ".player-0-panel" ).classList.toggle( "active" );
-    document.querySelector( ".player-1-panel" ).classList.toggle( "active" );
-
-    document.querySelector( ".dice" ).style.display = "none";
+  nextPlayer();
   }
 
+});
+
+document.querySelector( ".btn-hold" ).addEventListener( "click", function() {
+  // 1. Add current round score to global score
+  scores[activePlayer] += roundScore;
+  // 2. Update the UI
+  document.querySelector( "#score-" + activePlayer ).textContent = scores[activePlayer];
+  // 3. Check if player won the game
+  if ( scores[activePlayer] >= 100 ) {
+    document.querySelector( "#name-" + activePlayer ).textContent = "Winner!";
+    document.querySelector( ".dice" ).style.display = "none";
+    document.querySelector( ".player-" + activePlayer + "-panel" ).classList.add( "winner" );
+    document.querySelector( ".player-" + activePlayer + "-panel" ).classList.remove( "active" );
+  } else {
+    nextPlayer();
+  };
+
 })
+
+function nextPlayer() {
+  // Next Player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById( "current-0" ).textContent = "0";
+  document.getElementById( "current-1" ).textContent = "0";
+
+  // document.querySelector( ".player-0-panel" ).classList.remove( "active" );
+  // document.querySelector( ".player-1-panel" ).classList.add( "active" );
+
+  document.querySelector( ".player-0-panel" ).classList.toggle( "active" );
+  document.querySelector( ".player-1-panel" ).classList.toggle( "active" );
+
+  document.querySelector( ".dice" ).style.display = "none";
+}
